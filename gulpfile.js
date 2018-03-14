dd0493b204074789ab7ab862a6702077967e5538
Mohamed Moughamir <moughamir@gmail.com>
2018-03-14 18:45:57 +0000

    Mobile view for items

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
-const gulp       = require('gulp');
-const sass       = require('gulp-sass');
-const ap         = require('gulp-autoprefixer');
-const clean      = require('gulp-clean-css');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const ap = require('gulp-autoprefixer');
+const clean = require('gulp-clean-css');
 const sourceMaps = require('gulp-sourcemaps');
-const imagemin   = require('gulp-imagemin');
-const bsync      = require('browser-sync');
+const imagemin = require('gulp-imagemin');
+const bsync = require('browser-sync');
 
 const settings = {
-  sourceStyle     : 'src/sass/**/*',
-  targetStyle     : 'build/css/',
-  assetsSource    : 'src/assets/*',
-  assetsTarget    : 'build/assets/',
-  sassOptions     : {
+  sourceStyle: 'src/sass/**/*',
+  targetStyle: 'build/css/',
+  assetsSource: 'src/assets/*',
+  assetsTarget: 'build/assets/',
+  sassOptions: {
     errLogToConsole: true,
     outputStyle: 'expanded'
   },
-  autoprefixerOptions : {
+  autoprefixerOptions: {
     browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
   }
 }
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
   return gulp
-  .src(settings.sourceStyle)
-  .pipe(sourceMaps.init())
-  .pipe(sass(settings.sassOptions).on('error', sass.logError))
-  .pipe(ap(settings.autoprefixerOptions))
-  .pipe(clean({
-    compatibility: 'ie8'
-  }))
-  .pipe(sourceMaps.write())
-  .pipe(gulp.dest(settings.targetStyle));
+    .src(settings.sourceStyle)
+    .pipe(sourceMaps.init())
+    .pipe(sass(settings.sassOptions).on('error', sass.logError))
+    .pipe(ap(settings.autoprefixerOptions))
+    .pipe(clean({
+      compatibility: 'ie8'
+    }))
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest(settings.targetStyle));
 });
 
 gulp.task('imagemin', () =>
@@ -39,20 +39,27 @@ gulp.task('imagemin', () =>
   .pipe(gulp.dest(settings.assetsTarget))
 );
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   return gulp
-  .watch([settings.sourceStyle], ['sass'])
-  .on('change', function(event){
-    console.log('File ' + event.path + ' was ' + event.type + ' , running tasks...');
-  });
+    .watch([settings.sourceStyle], ['sass'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ' , running tasks...');
+    });
 });
 
-gulp.task('sync', function() {  
-  bsync.init([settings.targetStyle+'*.css', 'build/js/*.js', 'build/*.html'], {
-      server: {
-          baseDir: "./build/"
+gulp.task('sync', function() {
+  bsync.init([settings.targetStyle + '*.css', 'build/js/*.js', 'build/*.html'], {
+    server: {
+      baseDir: "./build/"
+    },
+    port: 8080,
+    ui: {
+      port: 8081,
+      weinre: {
+        port: 8082
       }
+    }
   });
 });
 
-gulp.task('default', ['sass','watch', 'sync']);
+gulp.task('default', ['sass', 'watch', 'sync']);
